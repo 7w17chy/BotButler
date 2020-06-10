2986fd341eec88a3a86ceb3ef6536f314ba60b99
Max Nerius <mnerius@gmx.de>
2020-06-10 13:59:44 +0200

    initial commit

---
+++ src/main/java/de/m_n_n/bot_butler/App.java
@@ -0,0 +1,11 @@
+package de.m_n_n.bot_butler;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
