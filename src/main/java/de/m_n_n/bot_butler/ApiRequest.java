5c07bca92d207e38edc54563750454f57b93890c
Max Nerius <mnerius@gmx.de>
2020-06-11 23:19:19 +0200

    http requests work now

--- src/main/java/de/m_n_n/bot_butler/ApiRequest.java
+++ src/main/java/de/m_n_n/bot_butler/ApiRequest.java
@@ -1,13 +1,14 @@
 package de.m_n_n.bot_butler;
 
 import java.collections.HashMap;
+import java.net.URL;
 
 public class ApiRequest {
-    private String m_url;
+    private URl m_url;
     private HashMap<String, String> m_reqParams;
 
     ApiRequest(String url, HashMap<String, String> req_params) {
-        m_url = url;
+        m_url = new URL(url);
         m_reqParams = new HashMap<String, String>();
     }
 
--- src/main/java/de/m_n_n/bot_butler/ApiRequestHandler.java
+++ src/main/java/de/m_n_n/bot_butler/ApiRequestHandler.java
@@ -1,6 +1,9 @@
 package de.m_n_n.bot_butler;
 
-import java.net.*;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 
 public class ApiRequestHandler {
     private ApiRequest m_req;
@@ -10,9 +13,18 @@ public class ApiRequestHandler {
     }
 
     private JsonObject apiRequest() {
-        URL api_url = new URL(m_req.getUrl());
-        HttpURLConnection con = (HttpURLConnection) api_url.openConnection();
-        con.setRequestMethod();
+        String result = "";
+        try {
+            InputStream stream = m_req.openStream();
+            // vorsicht ist die Muttter der Porzellankiste
+            byte[] bytes = new byte[stream.available() * 2];
+            stream.read(bytes);
+            result = new String(arr, StandardCharsets.UTF_8);
+            stream.close();
+        } catch (IOException e) {
+            // we've got a problem
+            e.printStackTrace();
+        }
         return new JsonObject();
     }
 
